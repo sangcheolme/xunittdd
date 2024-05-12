27edf3f2283388676e2ca4e546f8c278dd58ca5b
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-05-12 17:14:29 +0900

    feat: 테스트 전에 실행되는 beforeEach 메서드 개선

--- src/xunit/TestCase.java
+++ src/xunit/TestCase.java
@@ -7,6 +7,7 @@ import java.lang.reflect.Method;
  * 생성자에 이름을 넣고 run을 하면 그 이름의 메서드를 실행하는 TestCase 클래스
  */
 public class TestCase {
+
 	protected final String name;
 
 	public TestCase(String name) {
--- src/xunit/WasRun.java
+++ src/xunit/WasRun.java
@@ -2,19 +2,18 @@ package xunit;
 
 public class WasRun extends TestCase {
 
-	public boolean wasRun;
-	public boolean wasBeforeEach;
-
-	@Override
-	public void beforeEach() {
-		wasBeforeEach = true;
-	}
+	public String log;
 
 	public WasRun(String name) {
 		super(name);
 	}
 
+	@Override
+	public void beforeEach() {
+		log = "beforeEach";
+	}
+
 	public void testMethod() {
-		wasRun = true;
+		log += " testMethod";
 	}
 }
