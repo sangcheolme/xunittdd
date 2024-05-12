61dee5ca1834be6c7406e94ce4911992173761e5
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-05-12 20:21:49 +0900

    fix: test 수집 수정

--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -3,12 +3,14 @@ package xunit;
 public class XUnitTest {
 
 	public static void main(String[] args) {
+		TestSuite suite = new TestSuite();
+		suite.add(new TestCaseTest("testTemplateMethod"));
+		suite.add(new TestCaseTest("testResult"));
+		suite.add(new TestCaseTest("testFailedResultFormating"));
+		suite.add(new TestCaseTest("testFailedResult"));
+		suite.add(new TestCaseTest("testSuite"));
 		TestResult result = new TestResult();
-		new TestCaseTest("testTemplateMethod").run(result);
-		new TestCaseTest("testResult").run(result);
-		new TestCaseTest("testFailedResultFormating").run(result);
-		new TestCaseTest("testFailedResult").run(result);
-		new TestCaseTest("testSuite").run(result);
+		suite.run(result);
 		System.out.println(result.getSummary());
 	}
 }
