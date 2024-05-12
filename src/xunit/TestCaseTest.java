f1a1c92e2543f7fd91bbed1a6f0d23484a4eda9c
sang-cheol Park <127950374+sangcheolme@users.noreply.github.com>
2024-05-12 16:11:45 +0900

    feat: 테스트 자동화를 위한 Assert 클래스 구현

--- src/xunit/TestCaseTest.java
+++ src/xunit/TestCaseTest.java
@@ -1,14 +1,17 @@
 package xunit;
 
+import static xunit.Assert.*;
+
 public class TestCaseTest extends TestCase {
+
 	public TestCaseTest(String name) {
 		super(name);
 	}
 
 	public void testRunning() {
 		WasRun wasRun = new WasRun("testMethod");
-
+		assertEquals(false, wasRun.wasRun); // 실행 전에는 false
 		wasRun.run();
-		assert wasRun.wasRun;
+		assertEquals(true, wasRun.wasRun); // 실행 후에는 true
 	}
 }
--- src/xunit/XUnitTest.java
+++ src/xunit/XUnitTest.java
@@ -3,9 +3,6 @@ package xunit;
 public class XUnitTest {
 
 	public static void main(String[] args) {
-		WasRun wasRun = new WasRun("testMethod");
-		System.out.println(wasRun.wasRun); // 실행 전에는 false
-		wasRun.run();
-		System.out.println(wasRun.wasRun); // 실행 후에는 true
+		new TestCaseTest("testRunning").run();
 	}
 }
